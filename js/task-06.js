daae996c406d7b783a387bc42b14e0946762626d
Maksym Shumeiko <kkiilleerraa@mail.ru>
2022-05-26 11:26:54 +0300

    fix2

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,7 @@
 const input = document.querySelector('#validation-input')
 const value = input.dataset.length
 input.addEventListener("blur", (event) => {
-    if (event.currentTarget.value.length > value) {
+    if (event.currentTarget.value.length != value) {
         input.classList.add("invalid")
         input.classList.remove("valid")
     } else {
