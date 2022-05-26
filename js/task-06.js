d5b83d718a3db4426b3f1bed8ee8208a0bc3d60e
Maksym Shumeiko <kkiilleerraa@mail.ru>
2022-05-26 11:30:01 +0300

    fix-fix2

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,13 @@
 const input = document.querySelector('#validation-input')
-const value = input.dataset.length
+const value = Number(input.dataset.length)
+console.log(value)
 input.addEventListener("blur", (event) => {
-    if (event.currentTarget.value.length != value) {
+    if (event.currentTarget.value.length !== value) {
+        console.log(event.currentTarget.value.length)
         input.classList.add("invalid")
         input.classList.remove("valid")
     } else {
+        console.log(event.currentTarget.value.length)
         input.classList.add("valid")
         input.classList.remove("invalid")
     }
